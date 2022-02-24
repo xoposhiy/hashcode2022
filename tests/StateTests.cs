ecc6a765f6f951c60e579b10bd27d871505593d1
krutovskii <krutovskii@skbkontur.ru>
2022-02-25 00:12:30 +0500

    Stupid tests

--- tests/StateTests.cs
+++ tests/StateTests.cs
@@ -29,4 +29,29 @@ public class StateTests
         var actual = state.GetPossibleProjectsToStart().ToList();
         Console.WriteLine(actual.StrJoin("\n"));
     }
+
+    [TestCase("a_an_example")]
+    [TestCase("b_better_start_small")]
+    [TestCase("c_collaboration")]
+    [TestCase("d_dense_schedule")]
+    [TestCase("e_exceptional_skills")]
+    [TestCase("f_find_great_mentors")]
+    public void StupidGreedyWithStupidEstimator(string name)
+    {
+        var stupidEstimator = new StupidEstimator();
+        var problem = new ProblemReader().ReadByName(name);
+        var state = new State(problem);
+        
+        var greedySolver = new GreedySolver(stupidEstimator);
+
+        while (true)
+        {
+            var singleMoveSolution = greedySolver.GetSolutions(state, int.MaxValue).FirstOrDefault();
+            if (singleMoveSolution == default) break;
+            state.Projects.Add(singleMoveSolution.Move);
+        }
+
+        Console.WriteLine(stupidEstimator.GetScore(state));
+        Console.WriteLine(state);
+    }
 }
