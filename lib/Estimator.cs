e3848e21d5076ccd156116a779527619b6506324
krutovskii <krutovskii@skbkontur.ru>
2022-02-24 23:48:21 +0500

    Greedy + estimator

---
+++ lib/Estimator.cs
@@ -0,0 +1,9 @@
+namespace bot;
+
+public class Estimator : IEstimator<State>
+{
+    public double GetScore(State state)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ lib/GreedySolver.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace bot;
+
+public class GreedySolver : AbstractGreedySolver<State, Project>
+{
+    public GreedySolver(IEstimator<State> estimator) : base(estimator)
+    {
+    }
+
+    protected override State ApplyMove(State problem, Project move)
+    {
+        var copy = problem.Clone();
+        copy.Projects.Add(move);
+        return copy;
+    }
+
+    protected override IEnumerable<Project> GetMoves(State problem)
+    {
+        return problem.GetPossibleProjectsToStart();
+    }
+}
